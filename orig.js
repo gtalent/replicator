c6d4e84f7d6bbf8c3da612766097d53348524bb5
Gary Talent <gtalent2@gmail.com>
2014-06-18 17:48:19 -0500

    Reduced size of JS version.

--- orig.js
+++ orig.js
@@ -1,1 +1,1 @@
-q = String.fromCharCode(34);v = "q = String.fromCharCode(34);v = narf;console.log(v.replace('narf', q + v + q))";console.log(v.replace('narf', q + v + q))
+q=String.fromCharCode(34);v="q=String.fromCharCode(34);v=z;console.log(v.replace('z',q+v+q))";console.log(v.replace('z',q+v+q))
